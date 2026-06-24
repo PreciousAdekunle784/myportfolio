fcee774542a8d3f8e3a2a3031db94b7ff9917b0b
Precious Adekunle <preciousadekunle784@gmail.com>
2026-06-24 14:13:02 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -150,8 +150,8 @@ window.addEventListener('scroll', parallaxOrbs);
 
 // 7. Enhanced Scroll Reveal with Stagger
 const observerOptions = {
-    threshold: 0.1,
-    rootMargin: '0px 0px -50px 0px'
+    threshold: 0.15,
+    rootMargin: '0px 0px -100px 0px'
 };
 
 const observer = new IntersectionObserver((entries) => {
